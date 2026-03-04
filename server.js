e4469b314847841c64fa83b3bac2ba5fe9a0d3c3
Anshuman56 <anshumandehury@yandex.com>
2026-03-04 10:43:54 +0530

    add the mongodb

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 let express = require('express');
 let app = express();
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (request, response) =>{
     response.sendFile(__dirname + '/index.html')
 })
 
+app.post('/addbook', (request, response) =>{
+    console.log(request.body)
+})
 app.listen(8000, (request, response) => {
-    console.log('go to localhost//: 8000')
+    console.log('go to localhost//:8000')
 })
