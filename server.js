1b1dd7b0f1feb336e0e232c2b2e14575e725c8d6
Anshuman56 <anshumandehury@yandex.com>
2026-03-06 11:42:12 +0530

    add functionlty to add book to database collection

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 let express = require('express');
 let app = express();
+const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
 
 app.use(express.urlencoded({extended: true}))
+
+
+MongoClient.connect(process.env.MONGO_URL)
+.then( client => {
+    console.log('Connected to the database');
+    const db = client.db('books-list');
+    const booksCollection = db.collection('books');
+
+    app.post('/addbook', (request, response) =>{
+        booksCollection.insertOne(request.body)
+        .then(result => response.redirect('/'))
+        .catch(err => console.error(err))
+    })
+})
+.catch(err => console.error(err))
 
 app.get('/', (request, response) =>{
     response.sendFile(__dirname + '/index.html')
 })
 
-app.post('/addbook', (request, response) =>{
-    console.log(request.body)
-})
+
+
 app.listen(8000, (request, response) => {
     console.log('go to localhost//:8000')
 })
